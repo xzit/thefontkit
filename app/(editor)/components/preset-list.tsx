86aaf9da3e8705b1379422a9bab13941f16cd4d0
Xzit <xzit@hotmail.co.jp>
2025-08-13 21:19:56 -0600

    Initial release

--- app/(editor)/components/preset-list.tsx
+++ app/(editor)/components/preset-list.tsx
@@ -43,6 +43,7 @@ export default function PresetList() {
         });
       },
     );
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
   return (
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -18,34 +18,35 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar variant="floating" {...props}>
       <SidebarHeader>
-        <Link
-          href="/"
-          className="font-heading flex items-center gap-2.5 p-2 text-2xl font-bold"
-        >
-          <div className="bg-sidebar-primary text-sidebar-primary-foreground dark:text-sidebar flex aspect-square size-10 items-center justify-center rounded-lg">
-            <svg
-              width={24}
-              height={24}
-              fill="currentColor"
-              clipRule="evenodd"
-              fillRule="evenodd"
-              strokeLinejoin="round"
-              strokeMiterlimit="2"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="m0 .18.019-.12h.043c.027 0 .049-.007.066-.021s.028-.033.031-.058l.045-.281h-.152l.022-.135h.152l.02-.125c.005-.034.018-.064.038-.09.02-.025.045-.045.076-.059s.065-.021.103-.021h.137l-.021.13h-.132c-.013 0-.025.004-.034.012-.009.009-.015.02-.018.033l-.019.12h.195l-.022.135h-.195l-.046.286c-.01.06-.038.107-.084.142-.047.035-.105.052-.176.052z"
-                fillRule="nonzero"
-                transform="matrix(26.66664 0 0 26.3736 4.000000285 19.25276643)"
-              />
-            </svg>
-          </div>
-          <div className="flex w-full items-center justify-between">
-            <span>{t("name")}</span>
+        <div className="font-heading flex items-center gap-2.5 p-2 text-2xl font-bold">
+          <Link href="/">
+            <div className="bg-sidebar-primary text-sidebar-primary-foreground dark:text-sidebar flex aspect-square size-10 items-center justify-center rounded-lg">
+              <svg
+                width={24}
+                height={24}
+                fill="currentColor"
+                clipRule="evenodd"
+                fillRule="evenodd"
+                strokeLinejoin="round"
+                strokeMiterlimit="2"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m0 .18.019-.12h.043c.027 0 .049-.007.066-.021s.028-.033.031-.058l.045-.281h-.152l.022-.135h.152l.02-.125c.005-.034.018-.064.038-.09.02-.025.045-.045.076-.059s.065-.021.103-.021h.137l-.021.13h-.132c-.013 0-.025.004-.034.012-.009.009-.015.02-.018.033l-.019.12h.195l-.022.135h-.195l-.046.286c-.01.06-.038.107-.084.142-.047.035-.105.052-.176.052z"
+                  fillRule="nonzero"
+                  transform="matrix(26.66664 0 0 26.3736 4.000000285 19.25276643)"
+                />
+              </svg>
+            </div>
+          </Link>
+          <div className="flex w-full items-center justify-start gap-2.5">
+            <Link href="/">
+              <span>{t("name")}</span>
+            </Link>
             <span className="text-xs font-normal">v1.0.1</span>
           </div>
-        </Link>
+        </div>
       </SidebarHeader>
       <SidebarContent>
         <Editor />
